167f5eecf332f96026f8212d61745e5349f6e3a8
bkfwebdev <bryant.feld@gmail.com>
2017-07-15 23:05:17 -0400

    cannot pass data out of twit.get

--- src/app.js
+++ src/app.js
@@ -3,49 +3,74 @@
 const express = require("express");
 const pug = require("pug");
 const twit = require("twit");
-// const config = require("./config");
+const config = require("../config");
 var myApp = new express();
 var myContent = {
-  myTimeline : [],
-  myFollowers : [],
-  myMessages : []
+  myTweets:[],
+  myFollowers:[],
+  myMessages:[]
 }
 const myhttps = require("https");
-const timeLineTarget = "timelineapi";
-const followersTarget = "followersapi";
-const messagesTarget = "messagesapi";
-// var myTwit = new twit(config); 
-var timeLineData = "data did not pass";
+const timeLineTarget = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bkfwebdev&count=5";
+const followersTarget = "https://api.twitter.com/1.1/followers/list.json?cursor=-1&screen_name=bkfwebdev&skip_status=true&include_user_entities=false";
+const messagesTarget = "https://api.twitter.com/1.1/direct_messages.json?count=5";
+var myTwit = new twit(config); 
+var myStream = myTwit.stream("bkfwebdev");
 myApp.use(express.static(__dirname + '/public'));
-// var myPug = new pug();
-
 //---------------------------------------------------------------------
 // Main program logic
-
-// get timeline and update
-// console.log("you should see a data dump before the server launches...");
-var templateObject = {tweet1:"this is a test of the emergency broadcast system", 
-                    tweet2:"You know what's more important than throwing money at the strip club?, credit",
-                    tweet3:"I think my mind's playing tricks on me...",
-                    tweet4:"Wu Tang Clan , Wu Tang Clan and Iron Man...",
-                    tweet5:"Easily I approach, the microphone because I aint no joke...",
-                    background: "url(https://pbs.twimg.com/profile_images/856254755760373765/7HCLCibc_400x400.jpg)"
+ var templateTestObject = {tweets:["this is a test of the emergency broadcast system", 
+                                    "You know what's more important than throwing money at the strip club?, credit",
+                                    "I think my mind's playing tricks on me...",
+                                    "Wu Tang Clan , Wu Tang Clan and Iron Man...",
+                                    "Easily I approach, the microphone because I aint no joke...",
+                                    "background-image:url(https://pbs.twimg.com/profile_images/856254755760373765/7HCLCibc_400x400.jpg)"
+ ]
 }
 
-//var myStyleObject = {background-image: "url(https://pbs.twimg.com/profile_images/856254755760373765/7HCLCibc_400x400.jpg)"}
+ myTwit.get(timeLineTarget,function(req,res){
+    console.log("inside twit get call ");
+    console.dir(res); 
+    for(var x=0; x<=4; x++){
+      myContent.myTweets[x] = res[x].text;
+      console.log(myContent.myTweets[x]);
+    }
+ });
 
-var getSomeData = function (dataTarget){
-let someData = null;
-myTwit.get(dataTarget, function(req,res){
-  return res;
 
-});
-return someData;
+/* var getSomeFollowers = function (dataTarget,dataObject){   
+  dataObject = myTwit.get(dataTarget,function(req,res){
+    console.log("inside twit get call "+ dataTarget);
+    console.dir(res); 
+    dataObject = res;
+  });
 }
 
-// timeLineData = getSomeData(timeLineTarget);
+var getSomeMessages = function (dataTarget,dataObject){
+  dataObject = myTwit.get(dataTarget,function(req,res){
+    console.log("inside twit get call "+ dataTarget);
+    console.dir(res); 
+    dataObject = res;
+  });
+}
 
-// console.dir(timeLineData);
+
+
+function ornithologist(){
+  
+  console.log("you just tweeted ");
+  
+}
+
+myStream.on("tweet",ornithologist);
+
+myContent.myFollowers = getSomeData(followersTarget,myContent.myFollowers);
+console.dir(myContent.myFollowers)
+myContent.myMessages = getSomeData(messagesTarget,myContent.myMessages);
+console.dir(myContent.myMessages);
+myContent.myTimeline = getSomeData(timeLineTarget,myContent.myTimeline);
+console.dir(myContent.myTimeline);
+*/
 
 myApp.listen(3000, function (){
 console.log('front-end app listening on port 3000!');
@@ -53,32 +78,11 @@ console.log('front-end app listening on port 3000!');
 myApp.set("view engine", "pug");
 myApp.set("views", __dirname + "/views"); 
 myApp.get("/",function(req,res){
-  res.render("testindex",templateObject);
+  res.render("testindex",myContent);
 })
  
 
 
 
- 
-/*
-myApp.get("/",function(req,res){
-  res.render("index");
-})
 
-get followers and update
-myApp.get("/",function(req,res){
-  res.render("index");
-})
-
-get messages and update
-myApp.get("/",function(req,res){
-  res.render("index");
-})
-followers - > https://dev.twitter.com/rest/reference/get/followers/list
-direct messages - > https://dev.twitter.com/rest/reference/get/direct_messages
-timeline - > https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-
-
-
-*/
 
